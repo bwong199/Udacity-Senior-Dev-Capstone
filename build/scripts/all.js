d28072b46470be7e980c711ef3cbfea823c46526
bwong199 <bwong199@my.bcit.ca>
2016-07-20 22:06:17 -0600

    Removed the New York city placeholder on home screen

--- build/scripts/all.js
+++ build/scripts/all.js
@@ -4,7 +4,7 @@ var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 // SERVICES 
 
 weatherApp.service('cityService', function(){
-	this.city = "New York, NY";
+	this.city;
 
 
 })
